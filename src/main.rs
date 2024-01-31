b53518c35a7a23aa33543c1011b6dafeaac99755
iwyatt <7514713+iwyatt@users.noreply.github.com>
2024-01-30 18:25:58 -0800

    impl player keyboard control

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main () {
     App::new()
     .add_plugins((DefaultPlugins, TerminalPlugin))
     .add_systems(Startup, setup)
-    .add_systems(Update, walk)
+    .add_systems(Update, (player_walk, npc_walk))
     .add_systems(Update, tick)
     .run();
 }
@@ -65,7 +65,7 @@ fn tick(mut query_terminal: Query<&mut Terminal>, query_entities: Query<(&Positi
     //terminal.put_string([4,1], "Updates")
 }
 
-fn walk(mut query_walkers: Query<(&mut Position, &Enemy)>) {
+fn npc_walk(mut query_walkers: Query<(&mut Position, &Enemy)>) {
     query_walkers.iter_mut().for_each(|(mut p,_)| {
         p.x = p.x + 1;
         if p.x <= 1 {
@@ -73,6 +73,55 @@ fn walk(mut query_walkers: Query<(&mut Position, &Enemy)>) {
         }
     }
     );
+}
+
+fn player_walk(input: Res<Input<KeyCode>>, mut player_pos: Query<(&Player, &mut Position)>){
+    let move_input = read_movement(input);
+    if move_input.cmpeq(IVec2::ZERO).all() {
+        return;
+    }
+
+    let (player, mut pos) = player_pos.iter_mut().nth(0).map(|(player, mut pos)|(player, pos)).unwrap();
+    
+    let curr = IVec2::new(pos.x, pos.y);
+    let next = curr + move_input;
+    pos.x = next.x;
+    pos.y = next.y;
+}
+
+// an IVec2 is a 2-dimensional vector (direction and distance for x and y both)
+fn read_movement(input: Res<Input<KeyCode>>) -> IVec2 {
+    let mut p = IVec2::ZERO;
+
+    if input.just_pressed(KeyCode::Numpad1) || input.just_pressed(KeyCode::Z) {
+        p.x = -1;
+        p.y = -1;
+    }
+    if input.just_pressed(KeyCode::Numpad2) || input.just_pressed(KeyCode::X) || input.just_pressed(KeyCode::Down) {
+        p.y = -1;
+    }
+    if input.just_pressed(KeyCode::Numpad3) || input.just_pressed(KeyCode::C) {
+        p.x = 1;
+        p.y = -1;
+    }
+    if input.just_pressed(KeyCode::Numpad4) || input.just_pressed(KeyCode::A) || input.just_pressed(KeyCode::Left) {
+        p.x = -1;
+    }
+    if input.just_pressed(KeyCode::Numpad6) || input.just_pressed(KeyCode::D) || input.just_pressed(KeyCode::Right) {
+        p.x = 1;
+    }
+    if input.just_pressed(KeyCode::Numpad7) || input.just_pressed(KeyCode::Q) {
+        p.x = -1;
+        p.y = 1;
+    }
+    if input.just_pressed(KeyCode::Numpad8) || input.just_pressed(KeyCode::W) || input.just_pressed(KeyCode::Up) {
+        p.y = 1;
+    }
+    if input.just_pressed(KeyCode::Numpad9) || input.just_pressed(KeyCode::E) {
+        p.x = 1;
+        p.y = 1;
+    }
+    p
 }
 
 #[derive(Component)]
